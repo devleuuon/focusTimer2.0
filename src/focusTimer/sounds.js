a8227acd1ddaf869afa714afbf8a2287e48672d6
Lennon Fonseca <lennonfonsecaa@gmail.com>
2024-02-08 14:28:33 -0300

    add loop to sounds

--- src/focusTimer/sounds.js
+++ src/focusTimer/sounds.js
@@ -1,4 +1,11 @@
 export const buttonForest = new Audio('./assets/forest.wav')
+buttonForest.loop = true // o audio fica em loop sem parar
+
 export const buttonRain = new Audio('./assets/rain.wav')
-export const buttoncoffeeShop = new Audio('./assets/coffeeShop.wav')
-export const buttonfirePlace = new Audio('./assets/firePlace.wav')
+buttonRain.loop = true // o audio fica em loop sem parar
+
+export const buttonCoffeeShop = new Audio('./assets/coffeeShop.wav')
+buttonCoffeeShop.loop = true // o audio fica em loop sem parar
+
+export const buttonFirePlace = new Audio('./assets/firePlace.wav')
+buttonFirePlace.loop = true // o audio fica em loop sem parar
